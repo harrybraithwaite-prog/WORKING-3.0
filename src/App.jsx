37d7272d6beba3d503362ee5e0fbfa5e55dc2417
harrybraithwaite-prog <harry_braithwaite@hotmail.co.uk>
2025-08-07 16:58:13 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>Welcome to Cool Quote</h1>
+      <p>This is your starter template. Add your quoting features here.</p>
+    </div>
+  )
+}
